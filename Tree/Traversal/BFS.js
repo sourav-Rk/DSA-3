3981af702da1687dddf5e0bb271943965643efcd
SOURAV-RK <souravrk25@gmail.com>
2025-04-18 02:23:37 +0530

    added BFS

---
+++ Tree/Traversal/BFS.js
@@ -0,0 +1,77 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null
+    }
+
+    insert(value){
+        const newNode = new Node(value)
+        if(this.isEmpty()){
+            this.root = newNode
+        }else{
+            this.insertNode(this.root,newNode)
+        }
+    }
+
+    insertNode(root,newNode){
+        if(newNode.value < root.value){
+            if(root.left === null){
+                root.left = newNode
+            }else{
+                this.insertNode(root.left,newNode)
+            }
+        }else{
+            if(root.right === null){
+                root.right = newNode
+            }else{
+                this.insertNode(root.right,newNode)
+            }
+        }
+    }
+
+    search(root,value){
+        if(!root) return false
+        else{
+            if(root.value === value) return true
+            else if(value < root.value){
+                return this.search(root.left,value)
+            }else{
+                return this.search(root.right,value)
+            }
+        }
+    }
+
+    levelOrder(){
+        const queue = []
+        queue.push(this.root)
+        while(queue.length){
+            let curr = queue.shift()
+            console.log(curr.value)
+            if(curr.left) queue.push(curr.left)
+            if(curr.right) queue.push(curr.right)    
+        }
+    }
+
+    isEmpty(){
+        return this.root === null
+    }
+}
+
+const bst = new BinarySearchTree()
+bst.insert(50)
+bst.insert(30)
+bst.insert(20)
+bst.insert(10)
+bst.insert(70)
+bst.insert(40)
+bst.insert(45)
+bst.insert(60)
+bst.insert(80)
+bst.levelOrder()
