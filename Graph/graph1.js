6573cb02c654a1c57d80a55ac1044c771bcf3723
SOURAV-RK <souravrk25@gmail.com>
2026-01-02 21:14:58 +0530

    added function to clone a graph

--- Graph/graph1.js
+++ Graph/graph1.js
@@ -1,58 +1,74 @@
 class Graph {
-    constructor(){
-        this.adjacencyList = {}
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = new Set();
     }
-    
-    addVertex(vertex){
-        if(!this.adjacencyList[vertex]){
-            this.adjacencyList[vertex] = new Set()
-        }
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1]) {
+      this.addVertex(vertex1);
     }
-    
-    addEdge(vertex1,vertex2){
-        if(!this.adjacencyList[vertex1]){
-            this.addVertex(vertex1)
-        }
-        if(!this.adjacencyList[vertex2]){
-            this.addVertex(vertex2)
-        }
-        
-        this.adjacencyList[vertex1].add(vertex2)
-        this.adjacencyList[vertex2].add(vertex1)
-    }
-    
-    hasEdge(vertex1,vertex2){
-        return (
-             this.adjacencyList[vertex1].has(vertex2)
-              && this.adjacencyList[vertex2].has(vertex1)
-            )
+    if (!this.adjacencyList[vertex2]) {
+      this.addVertex(vertex2);
     }
 
-    removeEdge(vertex1,vertex2){
-        this.adjacencyList[vertex1].delete(vertex2)
-        this.adjacencyList[vertex2].delete(vertex1)
-    }
+    this.adjacencyList[vertex1].add(vertex2);
+    this.adjacencyList[vertex2].add(vertex1);
+  }
 
-    removeVertex(vertex){
-        if(!vertex) return
-        for(let adjacentVertex of this.adjacencyList[vertex]){
-            this.removeEdge(vertex,adjacentVertex)
-        }
-        delete this.adjacencyList[vertex]
+  hasEdge(vertex1, vertex2) {
+    return (
+      this.adjacencyList[vertex1].has(vertex2) &&
+      this.adjacencyList[vertex2].has(vertex1)
+    );
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].delete(vertex2);
+    this.adjacencyList[vertex2].delete(vertex1);
+  }
+
+  removeVertex(vertex) {
+    if (!vertex) return;
+    for (let adjacentVertex of this.adjacencyList[vertex]) {
+      this.removeEdge(vertex, adjacentVertex);
     }
-    
-    display(){
-        for(let vertex in  this.adjacencyList){
-            console.log(vertex + "->" + [...this.adjacencyList[vertex]])
-        }
+    delete this.adjacencyList[vertex];
+  }
+
+  display() {
+    for (let vertex in this.adjacencyList) {
+      console.log(vertex + "->" + [...this.adjacencyList[vertex]]);
     }
+  }
 }
 
-const graph = new Graph()
-graph.addVertex("A")
-graph.addVertex("B")
-graph.addVertex("C")
-graph.addEdge("A","B")
-graph.addEdge("B","C")
-graph.display()
-console.log(graph.hasEdge("A","C"))
+function deepcloneGraph(originalGraph) {
+  const clone = new Graph();
+
+  for (let vertex in originalGraph.adjacencyList) {
+    clone.addVertex(vertex);
+  }
+
+  for (let vertex in originalGraph.adjacencyList) {
+    for (let neighbour of originalGraph.adjacencyList[vertex]) {
+      clone.addEdge(vertex, neighbour);
+    }
+  }
+
+  return clone;
+}
+
+const graph = new Graph();
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
+graph.display();
+console.log(graph.hasEdge("A", "C"));
